c07a32a08a67310c3c1c5c6fac6cf2690241cab4
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-01-10 00:00:55 +0530

    Added project files

---
+++ jagrati/main/models.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    PROGRAMME_CHOICES = (
+        ('B.Tech.', 'B.Tech.'),
+        ('B.Des.', 'B.Des.'),
+        ('M.Tech.', 'M.Tech.'),
+        ('M.Des.', 'M.Des.'),
+        ('PhD', 'PhD'),
+    )
+
+    DEPARTMENT_CHOICES = (
+        ('CSE', 'CSE'),
+        ('ME', 'ME'),
+        ('ECE', 'ECE'),
+    )
+
+    user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    programme = models.CharField(max_length=10, choices=PROGRAMME_CHOICES)
+    discipline = models.CharField(max_length=3, choices=DEPARTMENT_CHOICES)
+    dob = models.DateField(blank=True, null=True)
+    batch = models.IntegerField(blank=True, null=True)
+    contact = models.IntegerField(blank=True, null=True)
+    address = models.CharField(max_length=50, blank=True, null=True)
+    status = models.CharField(max_length=128, blank=True, null=True)
+    is_contact_hidden = models.BooleanField(default=False)
+    display_picture = models.ImageField(upload_to='uploads/')
+
