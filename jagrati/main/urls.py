4e8661c3e4de471aa561e264833a2e8210a6103e
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-01-14 13:15:54 +0530

    Added views and urls for classes and users

--- jagrati/main/urls.py
+++ jagrati/main/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    
-]
+from .views import UserViewSet, ClassViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'classes', ClassViewSet)
+
+urlpatterns = router.urls
