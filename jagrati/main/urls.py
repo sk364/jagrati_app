cc3435ae0a27f700c905a339e55396168ce359de
Prakhar Sambhar <prakharsam97@gmail.com>
2018-01-16 13:54:50 +0530

    Adding serializers and urls

--- jagrati/main/urls.py
+++ jagrati/main/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, ClassViewSet
+from .views import UserViewSet, ClassViewSet, UserProfileViewSet, AttendaceViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'classes', ClassViewSet)
+router.register(r'user_profiles', UserProfileViewSet)
+router.register(r'attendance', AttendaceViewSet)
 
 urlpatterns = router.urls
