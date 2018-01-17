360f29b85980c97c9a8d19e441297017bf18d271
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2018-01-17 11:56:49 +0530

    Added new model UserClass

--- jagrati/main/models.py
+++ jagrati/main/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -30,11 +32,29 @@ class UserProfile(models.Model):
     is_contact_hidden = models.BooleanField(default=False)
     display_picture = models.ImageField(upload_to='uploads/')
 
+    def __str__(self):
+        return '{} - {} - {}'.format(self.user, self.programme, self.batch)
+
+
 class Class(models.Model):
     name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.name)
+
+
+class UserClass(models.Model):
+    user = models.ForeignKey(User, related_name='class_user', on_delete=models.CASCADE)
+    _class = models.ForeignKey(Class, related_name='user_class', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.user, self._class)
+
 
 class Attendance(models.Model):
     user = models.ForeignKey(User, related_name='user_attendance', on_delete=models.CASCADE) 
     _class = models.ForeignKey(Class, related_name='class_attendance', on_delete=models.CASCADE)
-    class_date = models.DateField()
+    class_date = models.DateField(default=datetime.date.today)
 
+    def __str__(self):
+        return '{} - {} - {}'.format(self.user, self._class, self.class_date)
